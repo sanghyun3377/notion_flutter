7706941a0659813bdc8a783c1a8aaf4b7658814d
sanghyun3377 <iccompany3377@gmail.com>
2023-12-28 14:59:22 +0900

    FutureBuilder 예시

--- lib/frist_page.dart
+++ lib/frist_page.dart
@@ -3,25 +3,42 @@ import 'package:flutter/material.dart';
 class FristPage extends StatelessWidget {
   const FristPage({super.key});
 
+  // 비동기 처리 (2초후 '데이터'를 리턴한다)
+  Future _future() async {
+    await Future.delayed(Duration(seconds: 2));
+    return '데이터';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GridView.builder(
-            itemCount: 100, //item 개수
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-              childAspectRatio: 2 / 3, //item 의 가로 1, 세로 2 의 비율
-              mainAxisSpacing: 20, //수평 Padding
-              crossAxisSpacing: 10, //수직 Padding
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              //item 의 반목문 항목 형성
-              return Container(
-                height: 100,
-                width: 100,
-                color: Colors.amber,
-                child: Text('$index'),
-              );
-            }));
+      body: Center(
+        child: FutureBuilder(
+            future: _future(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              //해당 부분은 data를 아직 받아 오지 못했을 때 실행되는 부분
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              //error가 발생하게 될 경우 반환하게 되는 부분
+              else if (snapshot.hasError) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Error: ${snapshot.error}', // 에러명을 텍스트에 뿌려줌
+                    style: TextStyle(fontSize: 15),
+                  ),
+                );
+              }
+              // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 부분
+              else {
+                return Text(
+                  snapshot.data.toString(), // 비동기 처리를 통해 받은 데이터를 텍스트에 뿌려줌
+                  style: TextStyle(fontSize: 20),
+                );
+              }
+            }),
+      ),
+    );
   }
 }
