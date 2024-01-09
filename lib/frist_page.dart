b3b80f83e0174c986dd8d8a2ffbf054797cb976f
sanghyun3377 <iccompany3377@gmail.com>
2024-01-09 15:10:02 +0900

    TextFormField 예시

--- lib/frist_page.dart
+++ lib/frist_page.dart
@@ -1,45 +1,57 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
-class FristPage extends StatelessWidget {
-  const FristPage({super.key});
-
-  // 2초마다 카운트를 세는 Stream<int>데이터타입으로 리턴해준다.
-  Stream<int> _addStreamValue() {
-    var res = Stream<int>.periodic(Duration(seconds: 2), (counter) => counter);
-    return res;
-  }
+class FristPage extends StatefulWidget {
+  const FristPage({Key? key}) : super(key: key);
 
   @override
+  State<FristPage> createState() => _FristPageState();
+}
+
+class _FristPageState extends State<FristPage> {
+  @override
   Widget build(BuildContext context) {
+    var controller = TextEditingController();
     return Scaffold(
       body: Center(
-          child: StreamBuilder(
-              stream: _addStreamValue(),
-              // initialData: '초기값',  앱을 처음시작하여 아무 데이터가 없을때 표시해줄 값(!snapshot.hasData 대신사용가능)
-              builder: (context, snapshot) {
-                //해당 부분은 data를 아직 받아 오지 못했을 때 실행되는 부분
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator();
-                }
-                //error가 발생하게 될 경우 반환하게 되는 부분
-                else if (snapshot.hasError) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Error: ${snapshot.error}', // 에러명을 텍스트에 뿌려줌
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  );
-                }
-                // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 부분
-                else {
-                  return Text(
-                    snapshot.data.toString(), // 비동기 처리를 통해 받은 데이터를 텍스트에 뿌려줌
-                    style: TextStyle(fontSize: 20),
-                  );
-                }
-              })),
+        child: TextFormField(
+          controller: controller,
+          onChanged: (text) {},
+          keyboardType: TextInputType.text,
+          maxLines: 1,
+          maxLength: 10, // 글자수 제한
+          validator: (value) {
+            // 유효성 검사
+            if (value == null || value.isEmpty) {
+              return '입력된 값이 없을때 나오는 텍스트';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            // 기본디자인
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                width: 2,
+                color: Color(0xFF4FB6B2),
+              ),
+            ),
+            // 눌렀을때 디자인
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                width: 2,
+                color: Colors.amber,
+              ),
+            ),
+            hintText: 'Search',
+            // 검색아이콘
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () async {},
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
